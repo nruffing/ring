d50b1a1b9817dba2a8fb61235b2d7277ccdb41de
Nicholas Ruffing <nicholasruffing70@gmail.com>
2021-01-04 19:13:40 -0500

    Create console app to refresh snapshots.

---
+++ src/RingService/Program.cs
@@ -0,0 +1,43 @@
+﻿using RingSharp;
+using Serilog;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Validation;
+
+namespace RingService
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log.json")
+                .CreateLogger();
+
+            try
+            {
+                var refreshToken = ConfigurationManager.AppSettings?["RingRefreshToken"];
+                Requires.NotNullOrWhiteSpace(refreshToken, nameof(refreshToken));
+
+                var client = new RingClient(refreshToken);
+
+                var devices = await client.GetAllDevicesAsync().ConfigureAwait(false);
+                var deviceIds = devices.Select(d => d.Id).ToArray();
+
+                while (true)
+                {
+                    await client.UpdateSnapshotAsync(deviceIds).ConfigureAwait(false);
+                    await Task.Delay(20000).ConfigureAwait(false);
+                    Console.WriteLine($"HEARTBEAT: {DateTime.Now}");
+                }
+            }
+            catch (Exception e)
+            {
+                log.Error(e, "There was an error updating snapshots.");
+            }
+        }
+    }
+}
